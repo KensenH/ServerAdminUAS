6cf8a2cee0fdf631ac7a86ff7a6468fd3d482865
KensenH <kensen.huang@gmail.com>
2021-02-15 23:29:38 -0800

    phpBARU

--- index.php
+++ index.php
@@ -5,16 +5,19 @@ if (!$link){
 echo "tidak bisa konek ke MySQL" . PHP_EOL;
 exit;
 }
-echo "Sukses: Koneksi telah terbuat!" . PHP_EOL;
+//echo "Sukses: Koneksi telah terbuat!" . PHP_EOL;
 echo "Host Information: " . mysqli_get_host_info($link) . PHP_EOL;
 $sql = "SELECT * FROM users";
+$counter = 0;
 $result = mysqli_query($link, $sql);
 
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
+    $counter = $counter + 1;
     echo "<br>id: " . $row["ID"]. "<br>" .  "Name: " . $row["Nama"]. "<br>" . "Kantor: " . $row["Kantor"]. "<br><br>";
-  }
+}
+ echo "<br>Jumlah User = " . $counter;
 } else {
   echo "0 results";
 }
